8059e24deceec193fe200bca739f833c1b356f72
Nikita Mihaylov <nicitamihaylov@yandex.ru>
2026-01-09 17:21:57 +0700

    Добавлены эндпоинты /api/courses и /api/users

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from api.routers import courses,users
 import uvicorn
 
 
 app = FastAPI()
 
+app.include_router(courses.router, prefix="/api/courses", tags=['courses'] )
+app.include_router(users.router, prefix="/api/users", tags=['users'])
+
+
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, host="localhost", port=3333)
